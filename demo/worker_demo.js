3cf9874d7240cf6503b835b5641f21cddcc4b61c
Unzor <71789103+Unzor@users.noreply.github.com>
2021-05-24 08:31:51 -0500

    Create worker_demo.js

---
+++ demo/worker_demo.js
@@ -0,0 +1,11 @@
+onmessage = function(e) {
+  console.log('Worker: Message received from main script');
+  const result = e.data[0] * e.data[1];
+  if (isNaN(result)) {
+    postMessage('Woo!');
+  } else {
+    const workerResult = 'Result: ' + result;
+    console.log('Worker: Posting message back to main script');
+    postMessage(workerResult);
+  }
+}
